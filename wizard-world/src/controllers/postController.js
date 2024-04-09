81e3b1e9ad34c50f21a48b33feb90547694213ae
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-04-09 19:38:02 +0300

    added create operation on creature

--- wizard-world/src/controllers/postController.js
+++ wizard-world/src/controllers/postController.js
@@ -1,15 +1,32 @@
 const router = require("express").Router();
+const creatureService = require("../services/creatureSevrice");
 
 router.get('/all', (req, res) => {
     res.render("post/all-posts")
 });
 
+
+
+
+
 router.get('/create', (req, res) => {
     res.render("post/create")
 });
 
+router.post('/create', async (req, res) => {
+
+    const { name, species, skin, eye, image, description } = req.body;
+    const payload = { name, species, skin, eye, image, description, owner:req.user };
+    await creatureService.create(payload);
+    res.redirect('/posts/all')
+});
+
+
+
 router.get('/profile', (req, res) => {
     res.render("post/profile")
 });
+
+
 
 module.exports = router
--- wizard-world/src/models/Creature.js
+++ wizard-world/src/models/Creature.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
 const creatureSchema = new mongoose.Schema({
-name:{ type: String, required :true},
-species:{ type: String, required :true},
-skinColor:{ type: String, required :true},
-eyeColor:{ type: String, required :true},
-image:{ type: String, required :true},
-description:{ type: String, required :true},
-votes: '',
-owner:''
-
+    name: { type: String, required: true },
+    species: { type: String, required: true },
+    skin: { type: String, required: true },
+    eye: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    votes: [{
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    }
 });
 
 
